613cd9e35575070e578520c67dad7a8c16e134e4
AlexandrRepkin <arepkin96@mail.ru>
2023-05-12 21:02:56 +0300

    TimerEnd

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,14 +1,16 @@
 import Timer from "../timer/timer";
 import AddExercise from "../addexercise/addexercise";
+import ExercisesMain from "../exercisesMain/exrcisesMain";
 import TimerSettings from "../app/timersettings/timersettings";
 
 const MainPage = () => {
     return(
-        <section className="main-page">
+        <section className="main-page" style={{display: 'flex', flexDirection: 'column'}}>
             <div className="container">
                 <Timer />
-                <AddExercise/>
             </div>
+            <ExercisesMain/>
+            <AddExercise/>
         </section>
     )
 }
